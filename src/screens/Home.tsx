601eb13355e4b66cc13187fd8957a0951e9ad743
Fabricyo Barreto <bass.barreto@gmail.com>
2023-02-25 15:10:09 -0300

    style: made the component GroupMuscles and the list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,52 @@
+import { GroupMuscles } from '@components/GroupMuscles';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base';
+import { useState } from 'react';
 
 export function Home() {
+  const [groupMusclesSelected, setGroupMusclesSelected] = useState('ombro');
+  const [groups, setGroups] = useState([
+    'costas',
+    'bíceps',
+    'tríceps',
+    'ombro',
+  ]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => {
+          return (
+            <GroupMuscles
+              name={item}
+              isActive={groupMusclesSelected === item}
+              onPress={() => setGroupMusclesSelected(item)}
+            />
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
