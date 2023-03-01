b68494d20ec084abd168ff5f389c51b3e8c83e47
Fabricyo Barreto <bass.barreto@gmail.com>
2023-02-28 21:25:49 -0300

    feat: configured the api service and made the logic to integrate to an API. made the signUp.

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -3,11 +3,14 @@ import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import axios from 'axios';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { api } from '@services/api';
+import { Alert } from 'react-native';
 
 type FormData = {
   name: string;
@@ -43,8 +46,15 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: FormData) {
-    console.log(data);
+  async function handleSignUp({ name, email, password }: FormData) {
+    try {
+      const response = await api.post('/users', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   }
 
   return (
