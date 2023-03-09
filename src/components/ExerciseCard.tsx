7fdd0c505478b7cd05babfa507b384d3a9f937f6
Fabricyo Barreto <bass.barreto@gmail.com>
2023-03-08 22:07:09 -0300

    feat: It was made functionality of list exercies by musculars groups

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,10 +1,17 @@
 import { Heading, HStack, Icon, Image, Text, VStack } from 'native-base';
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
 import { Entypo } from '@expo/vector-icons';
 
-type Props = TouchableOpacityProps;
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
-export function ExerciseCard({ ...rest }: Props) {
+import { api } from '@services/api';
+
+type Props = TouchableOpacityProps & {
+  data: ExerciseDTO;
+};
+
+export function ExerciseCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity {...rest}>
       <HStack
@@ -17,7 +24,7 @@ export function ExerciseCard({ ...rest }: Props) {
       >
         <Image
           source={{
-            uri: 'https://marcelogomespersonal.com/wp-content/uploads/2022/01/remada-curvada-com-barra-pegada-pronada-ou-pegada-supinada.jpg',
+            uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}`,
           }}
           alt="Remada Curvada"
           w={16}
@@ -28,10 +35,10 @@ export function ExerciseCard({ ...rest }: Props) {
         />
         <VStack flex={1}>
           <Heading color="white" fontSize="lg">
-            Remada Curvada
+            {data.name}
           </Heading>
           <Text color="gray.200" fontSize="sm" mt={1} numberOfLines={2}>
-            3 séries x 12 repetições
+            {data.series} séries x {data.repetitions} repetições
           </Text>
         </VStack>
         <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,13 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { FlatList, Heading, HStack, Text, useToast, VStack } from 'native-base';
 import { useCallback, useEffect, useState } from 'react';
 
 import { ExerciseCard } from '@components/ExerciseCard';
 import { GroupMuscles } from '@components/GroupMuscles';
 import { HomeHeader } from '@components/HomeHeader';
+import { Loading } from '@components/Loading';
+
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
@@ -14,17 +17,11 @@ import { AppError } from '@utils/AppError';
 
 export function Home() {
   const [groupMusclesSelected, setGroupMusclesSelected] = useState('Ombro');
+  const [isLoading, setIsLoading] = useState(true);
   const navigation = useNavigation<AppNavigatorRoutesProps>();
   const toast = useToast();
   const [groups, setGroups] = useState<Array<string>>([]);
-  const [exercises, setExercises] = useState([
-    'Puxada frontal',
-    'Remada curvada',
-    'Remada Unilateral',
-    'Levantamento terra',
-    'Supino Máquina',
-    'Rosca Scott',
-  ]);
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
 
   function handleOpenExerciseDetails() {
     navigation.navigate('exercise');
@@ -47,9 +44,37 @@ export function Home() {
     }
   }, [toast]);
 
+  const fetchExercisesByGroup = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.get(
+        `/exercises/bygroup/${groupMusclesSelected}`,
+      );
+      setExercises(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possivel carregar os exercícios';
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, [groupMusclesSelected, toast]);
+
   useEffect(() => {
     fetchGroups();
   }, [fetchGroups]);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchExercisesByGroup();
+    }, [fetchExercisesByGroup]),
+  );
 
   return (
     <VStack flex={1}>
@@ -79,26 +104,30 @@ export function Home() {
         minH={10}
       />
 
-      <VStack flex={1} px={8}>
-        <HStack justifyContent="space-between" mb={5}>
-          <Heading color="gray.200" fontSize="md">
-            Exercicios
-          </Heading>
-          <Text color="gray.200" fontSize="sm">
-            {exercises.length}
-          </Text>
-        </HStack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <VStack flex={1} px={8}>
+          <HStack justifyContent="space-between" mb={5}>
+            <Heading color="gray.200" fontSize="md">
+              Exercicios
+            </Heading>
+            <Text color="gray.200" fontSize="sm">
+              {exercises.length}
+            </Text>
+          </HStack>
 
-        <FlatList
-          data={exercises}
-          keyExtractor={item => item}
-          renderItem={({ item }) => (
-            <ExerciseCard onPress={handleOpenExerciseDetails} />
-          )}
-          showsVerticalScrollIndicator={false}
-          _contentContainerStyle={{ paddingBottom: 20 }}
-        />
-      </VStack>
+          <FlatList
+            data={exercises}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <ExerciseCard onPress={handleOpenExerciseDetails} data={item} />
+            )}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ paddingBottom: 20 }}
+          />
+        </VStack>
+      )}
     </VStack>
   );
 }
